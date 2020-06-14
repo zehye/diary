e09d89865a8c5d60b416452eea61a282beb14fee
znfod-dev <znfod.dev@gmail.com>
2020-06-14 18:25:39 +0900

    Disable KeyboardManager

--- Diary/SupportingFiles/AppDelegate.swift
+++ Diary/SupportingFiles/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import CoreData
-import IQKeyboardManagerSwift
 
 
 @UIApplicationMain
@@ -17,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        IQKeyboardManager.shared.enable = true
+        // IQKeyboardManager.shared.enable = true
 
         return true
     }
