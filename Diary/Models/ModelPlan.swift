3266a9edeecb8b4e201b5beb7280487a61e297e5
jihye <zehye.01@gmail.com>
2020-07-21 14:36:17 +0900

    modelplan

--- Diary/Models/ModelPlan.swift
+++ Diary/Models/ModelPlan.swift
@@ -8,6 +8,24 @@
 
 import UIKit
 
-class ModelPlan: NSObject {
+class ModelPlan: BaseModel {
 
+    // 제목
+    var title = String()
+    // 날짜
+    var date: Date = Date()
+    // 위치
+    var location = String()
+    // 공유 링크
+    var link = String()
+    // 메모
+    var memo = String()
+    
+    override init() {
+        super.init()
+    }
+    
+    override init(_ dictionary: Dictionary<String, Any>) {
+        super.init(dictionary)
+    }
 }
